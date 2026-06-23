52a5ed9ff54f2dcbdc3dbbd1ec2197a0fea27e95
bavakarni05 <bavakarnig@gmail.com>
2026-06-23 12:04:54 +0530

    modified:   backend/src/main/java/com/example/collegeportal/controller/HelloController.java

--- backend/src/main/java/com/example/collegeportal/controller/HelloController.java
+++ backend/src/main/java/com/example/collegeportal/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
  
 @RestController
-@CrossOrigin(origins = "https://benevolent-manatee-9c00b9.netlify.app", allowedHeaders = "*")
+@CrossOrigin(origins = "https://collegeportal2.netlify.app", allowedHeaders = "*")
 public class HelloController {
 
     @GetMapping("/api/health")
